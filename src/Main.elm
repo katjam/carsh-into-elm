d53e2126eccfa62556c5cfd06283e72c50d0c13d
katjam <katjamordaunt@gmail.com>
2021-12-10 09:47:11 +0000

    One file

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,80 @@
 module Main exposing (main)
 
-import Base exposing (Base)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
 
+
+-- Model
+
+
+type alias Model =
+    { value : Int
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { value = 0
+      }
+    , Cmd.none
+    )
+
+
+
+-- Update
+
+
+type Msg
+    = AddOne
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        AddOne ->
+            ( { model | value = model.value + 1 }
+            , Cmd.none
+            )
+
+
+
+-- View
+
+
+viewDocument : Model -> Browser.Document Msg
+viewDocument model =
+    { title = "Crash into Elm", body = [ view model ] }
+
+
+view : Model -> Html Msg
+view model =
+    div [ style "padding" "50px" ]
+        [ h1 [] [ text "My Buttons" ]
+        , div []
+            [ Html.button
+                [ Html.Events.onClick AddOne
+                ]
+                [ text "Add one!" ]
+            , hr [] []
+            , div [] [ text ("Value is " ++ String.fromInt model.value) ]
+            ]
+        ]
+
+
+
+-- Program
+
+
 type alias Flags =
     ()
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 main : Program Flags Model Msg
@@ -19,55 +85,3 @@ main =
         , subscriptions = subscriptions
         , view = viewDocument
         }
-
-
-type alias Model =
-    { base : Base
-    }
-
-
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( { base =
-            Base.init
-      }
-    , Cmd.none
-    )
-
-
-type Msg
-    = DoAction
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        DoAction ->
-            ( { model | base = { value = model.base.value + 1 } }
-            , Cmd.none
-            )
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
-
-
-viewDocument : Model -> Browser.Document Msg
-viewDocument model =
-    { title = "Kelm App", body = [ view model ] }
-
-
-view : Model -> Html Msg
-view model =
-    div [ style "padding" "50px" ]
-        [ h1 [] [ text "Base template" ]
-        , div []
-            [ Html.button
-                [ Html.Events.onClick DoAction
-                ]
-                [ text "Do action!" ]
-            , hr [] []
-            , Base.view model.base
-            ]
-        ]
